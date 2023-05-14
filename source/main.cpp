b6252efe11536c29f619eecc44052af9e736fb49
be <NULL>
2023-05-15 00:30:11 +0800

    新增多边形添加函数

--- source/main.cpp
+++ source/main.cpp
@@ -5,27 +5,7 @@
 #include "geoelement.h"
 #include "render.h"
 #include "balloperate.h"
-
-// 生成一个多边形
-polygonNode polygonGenerator(int xc, int yc, int edgeNum, int radius, int HP, double theta = 0)
-{
-	if (theta == 0) theta = PI / 180 * (rand() % 360);
-
-	polygonNode node;
-	node.edgeNum = edgeNum;
-	node.HP = HP;
-	node.xc = xc, node.yc = yc;
-	node.radius = radius;
-
-	for (int i = 0; i < edgeNum; i++)
-	{
-		node.pt[i].x = xc + radius * cos(theta);
-		node.pt[i].y = yc + radius * sin(theta);
-
-		theta += 2 * PI / edgeNum;
-	}
-	return node;
-}
+#include "mapgenerator.h"
 
 int main()
 {
---
+++ source/mapgenerator.h
@@ -0,0 +1,8 @@
+#ifndef _FILE_MAPGENERATOR_
+#define _FILE_MAPGENERATOR_
+
+#include "geoelement.h"
+
+polygonNode polygonGenerator(int xc, int yc, int edgeNum, int radius, int HP, double theta = 0);   // 生成一个多边形
+
+#endif
